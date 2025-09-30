8301641e7ce5487d1e2457697ef8674d07761486
danilojoncic <djoncic14122rn@raf.rs>
2025-09-30 22:05:22 +0200

    [PARSER] basic parsing of one file

--- data-creation/parser.py
+++ data-creation/parser.py
@@ -1,7 +1,31 @@
 #The purpose of this file is to parse the .xlsx files and convert them into single simple .csv
+import os
+
 import pandas as pd
 
 
+def parse_file(filename:str):
+    df = pd.read_excel(filename, engine="calamine",skiprows=3)
+    df = df.iloc[:, 1:]
 
-def parse():
+    df = df.iloc[:-4,:]
+
+    df.columns = df.columns.str.replace(r'[\r\n]+', ' ', regex=True).str.strip()
+    df = df.map(lambda x: str(x).replace("\r\n", " ").strip() if isinstance(x, str) else x)
+    print(df)
     pass
+
+
+
+
+def parse_directory():
+    pass
+
+
+
+def main():
+    parse_file("./files/leaderboard_20241110_220000.xlsx")
+
+
+if __name__ == '__main__':
+    main()
